5a9fe6af28be8d6d7abc24ae171a0f3ce5e129b8
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-20 18:17:57 +0100

    sistemazione file

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-// alert('ciao');
-// $('.fa-bars').click(
-//   function() {
-//     var visible = $('.hamburger-menu').hasClass('none');
-//     console.log(visible);
-//     if (visible) {
-//       $('.hamburger-menu').removeClass('none');
-//     } else {
-//       $('.hamburger-menu').addClass('none');
-//     }
-//   }
-// );
-
 $('.fa-bars').click(
   function() {
     $('.hamburger-menu').show()
